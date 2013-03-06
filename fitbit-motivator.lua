e0899c5da7ea26579b8cd1df9f0ef5f3b17aeefe
Matt McCormick <matt@mbmccormick.com>
2013-03-06 14:13:36 -0800

    Update fitbit-motivator.lua

--- fitbit-motivator.lua
+++ fitbit-motivator.lua
@@ -26,7 +26,29 @@ local sedentaryMinutes = storage.sedentaryMinutes or 0
 local minutes = tonumber(os.time() - lastCheck) / 60
 
 if (data.summary.sedentaryMinutes - sedentaryMinutes) / minutes > 0.75 then
-	alert.sms('You have been sedentary for over 75% of the last ' .. minutes .. ' minutes!')
+	local alarmTime = os.date('%H') .. ':' .. os.date('%M', os.time() + 600)
+	
+	local response = http.request {
+		url = 'http://api.fitbit.com/1/user/-/devices/tracker/1420941/alarms.json',
+		auth = {
+			oauth = {
+				consumertoken = CONSUMER_KEY,
+				consumersecret = CONSUMER_SECRET,
+				accesstoken = ACCESS_TOKEN,
+				tokensecret = TOKEN_SECRET
+			}
+		},
+		data = { 
+			time = alarmTime .. '+00:00',
+			enabled = 'true',
+			recurring = 'false',
+			weekDays = 'none',
+			label = 'WAKE UP',
+			snoozeLength = '5',
+			snoozeCount = '1',
+			vibe = 'DEFAULT'
+		}
+	}
 end
 
 storage.lastCheck = os.time()
