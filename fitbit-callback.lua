98a79d1d737b6549f589cf8f1ea298ec076361e5
Matt McCormick <matt@mbmccormick.com>
2013-03-06 11:11:01 -0800

    Update fitbit-callback.lua

--- fitbit-callback.lua
+++ fitbit-callback.lua
@@ -7,7 +7,7 @@ local TOKEN_SECRET = storage.tokensecret or nil
 local date = os.date('%Y') .. '-' .. os.date('%m') .. '-' .. os.date('%d')
 
 function check_battery()
-  local response = http.request {
+	local response = http.request {
 		url = 'http://api.fitbit.com/1/user/-/devices.json',
 		auth = {
 			oauth = {
