f2a16faf64ff6ba83c2f07f1536353ef61300722
Matt McCormick <matt@mbmccormick.com>
2013-03-06 11:14:43 -0800

    Create fitbit-battery.lua

---
+++ fitbit-battery.lua
@@ -0,0 +1,25 @@
+local CONSUMER_KEY = 'Fitbit Consumer Key'
+local CONSUMER_SECRET = 'Fitbit Consumer Secret'
+
+local ACCESS_TOKEN = storage.accesstoken or nil
+local TOKEN_SECRET = storage.tokensecret or nil
+
+local response = http.request {
+  url = 'http://api.fitbit.com/1/user/-/devices.json',
+	auth = {
+		oauth = {
+			consumertoken = CONSUMER_KEY,
+			consumersecret = CONSUMER_SECRET,
+			accesstoken = ACCESS_TOKEN,
+			tokensecret = TOKEN_SECRET
+		}
+	}
+}
+
+local data = json.parse(response.content)
+
+for key, value in pairs(data) do
+	if value.battery == 'Low' then
+		alert.email('Your Fitbit ' .. value.deviceVersion .. ' has a low battery.')
+	end
+end
