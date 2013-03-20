4895d61d85f0353cfcf44aa87fd6af86526f3aef
Matt McCormick <matt@mbmccormick.com>
2013-03-20 15:18:54 -0700

    Update fitbit-motivator.lua

--- fitbit-motivator.lua
+++ fitbit-motivator.lua
@@ -4,6 +4,14 @@ local CONSUMER_SECRET = 'Fitbit Consumer Secret'
 local ACCESS_TOKEN = storage.accesstoken or nil
 local TOKEN_SECRET = storage.tokensecret or nil
 
+local hour = tonumber(os.date('%H')) - 7
+
+if (hour < 9 or -- 9:00am pacific time
+    hour > 19) then -- 7:00pm pacific time
+	storage.sedentaryMinutes = 0
+	return
+end
+
 local date = os.date('%Y') .. '-' .. os.date('%m') .. '-' .. os.date('%d')
 
 local response = http.request {
@@ -25,30 +33,11 @@ local sedentaryMinutes = storage.sedentaryMinutes or 0
 
 local minutes = tonumber(os.time() - lastCheck) / 60
 
-if (data.summary.sedentaryMinutes - sedentaryMinutes) / minutes > 0.75 then
-	local alarmTime = os.date('%H') .. ':' .. os.date('%M', os.time() + 600)
-	
-	local response = http.request {
-		url = 'http://api.fitbit.com/1/user/-/devices/tracker/1420941/alarms.json',
-		auth = {
-			oauth = {
-				consumertoken = CONSUMER_KEY,
-				consumersecret = CONSUMER_SECRET,
-				accesstoken = ACCESS_TOKEN,
-				tokensecret = TOKEN_SECRET
-			}
-		},
-		data = { 
-			time = alarmTime .. '+00:00',
-			enabled = 'true',
-			recurring = 'false',
-			weekDays = 'none',
-			label = 'WAKE UP',
-			snoozeLength = '5',
-			snoozeCount = '1',
-			vibe = 'DEFAULT'
-		}
-	}
+local percentage = math.floor(((data.summary.sedentaryMinutes - sedentaryMinutes) / minutes) * 10)
+log(percentage)
+
+if percentage > 60 then
+	alert.sms('Wake up! You\'ve been sedentary for ' .. percentage .. '% of the last 2 hours.')
 end
 
 storage.lastCheck = os.time()
